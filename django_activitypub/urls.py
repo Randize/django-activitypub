27a31adac35603dd5407aef1421db09beefeafd2
Randize <eins32@hotmail.com>
2025-01-16 22:15:07 +0800

    Added @username path pointing to actor profile

--- django_activitypub/urls.py
+++ django_activitypub/urls.py
@@ -4,6 +4,7 @@ from django_activitypub.views import webfinger, profile, followers, inbox, outbo
 urlpatterns = [
     path('.well-known/webfinger', webfinger, name='activitypub-webfinger'),
     path('pub/<slug:username>', profile, name='activitypub-profile'),
+    path('@<slug:username>', profile, name='activitypub-profile'),
     path('pub/<slug:username>/followers', followers, name='activitypub-followers'),
     path('pub/<slug:username>/inbox', inbox, name='activitypub-inbox'),
     path('pub/<slug:username>/outbox', outbox, name='activitypub-outbox'),
