0e0b86ed04ef6d3574a9d705b6d89a3f382abbe4
Randize <eins32@hotmail.com>
2025-01-17 13:59:11 +0800

    Switches to better admin list_display for Note model

--- django_activitypub/admin.py
+++ django_activitypub/admin.py
@@ -6,4 +6,7 @@ from django_activitypub.models import LocalActor, RemoteActor, Follower, Note
 admin.site.register(LocalActor)
 admin.site.register(RemoteActor)
 admin.site.register(Follower)
-admin.site.register(Note)
+
+@admin.register(Note)
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ('id', 'content', 'updated_at')
