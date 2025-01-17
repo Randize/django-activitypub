6081526a27e54c2cd8200f4a122e9d4f9adb81a0
Randize <eins32@hotmail.com>
2025-01-17 14:06:54 +0800

    Admin list_display updates

--- django_activitypub/admin.py
+++ django_activitypub/admin.py
@@ -3,10 +3,16 @@ from django.contrib import admin
 from django_activitypub.models import LocalActor, RemoteActor, Follower, Note
 
 
-admin.site.register(LocalActor)
-admin.site.register(RemoteActor)
 admin.site.register(Follower)
 
 @admin.register(Note)
 class NoteAdmin(admin.ModelAdmin):
     list_display = ('id', 'content_url', 'updated_at')
+    
+@admin.register(LocalActor)
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ('id', 'preferred_username')
+    
+@admin.register(RemoteActor)
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'domain')
