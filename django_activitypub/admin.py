1dd9d0c6f9c6b02bd97578e7f9fccdce0ad5e5e2
Randize <eins32@hotmail.com>
2025-01-28 20:29:54 +0800

    admin: bug fix and ordering

--- django_activitypub/admin.py
+++ django_activitypub/admin.py
@@ -8,12 +8,12 @@ admin.site.register(Following)
 
 @admin.register(Note)
 class NoteAdmin(admin.ModelAdmin):
-    list_display = ('id', '__str__', 'updated_at')
+    list_display = ('__str__', 'id', 'tombstone', 'updated_at')
     
 @admin.register(LocalActor)
-class NoteAdmin(admin.ModelAdmin):
-    list_display = ('id', 'preferred_username')
+class LocalActorAdmin(admin.ModelAdmin):
+    list_display = ('preferred_username', 'id')
     
 @admin.register(RemoteActor)
-class NoteAdmin(admin.ModelAdmin):
-    list_display = ('id', 'username', 'domain')
+class RemoteActorAdmin(admin.ModelAdmin):
+    list_display = ('username', 'domain', 'id')
