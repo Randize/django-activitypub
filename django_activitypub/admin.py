8bcf946a8f8b594de35f5ead1db8326e418dcc0d
Randize <eins32@hotmail.com>
2025-01-29 19:01:02 +0800

    ImageAttachmentInline: hide clear link

--- django_activitypub/admin.py
+++ django_activitypub/admin.py
@@ -6,6 +6,15 @@ class ImageAttachmentInline(admin.TabularInline):
     model = ImageAttachment
     extra = 1 
 
+    def formfield_for_dbfield(self, db_field, **kwargs):
+        field = super().formfield_for_dbfield(db_field, **kwargs)
+        if db_field.name == "file":
+            field.widget.attrs.update({"class": "no-clearable-file"})  # Custom CSS
+        return field
+
+    class Media:
+        css = {"all": ("admin/css/hide_clear_link.css",)}  # Custom CSS file
+
 admin.site.register(Follower)
 admin.site.register(Following)
 
