5997210e7c1208df4de072cc9cf835ff192a7934
Randize <eins32@hotmail.com>
2025-01-31 21:49:29 +0800

    remote_redirect: update

--- django_activitypub/urls.py
+++ django_activitypub/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from django_activitypub.views import webfinger, profile, followers, inbox, outbox, hostmeta, nodeinfo, nodeinfo_links, notes, followings
+from django_activitypub.views import webfinger, profile, followers, inbox, outbox, hostmeta, nodeinfo, nodeinfo_links, notes, followings, remote_redirect
 
 urlpatterns = [
     path('.well-known/webfinger', webfinger, name='activitypub-webfinger'),
     path('.well-known/host-meta', hostmeta, name='activitypub-hostmeta'),
     path('.well-known/nodeinfo', nodeinfo_links, name='activitypub-nodeinfo'),
+    path('.well-known/redirect', remote_redirect, name='activitypub-redirect'),
     path('nodeinfo/<str:version>', nodeinfo, name='activitypub-nodeinfo'),
     path('pub/<slug:username>', profile, name='activitypub-profile'),
     path('@<slug:username>', profile, name='activitypub-profile-short'),
