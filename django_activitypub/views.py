d1c752ba61b0507e1ba0f5e6f464477888c6e8ee
Randize <eins32@hotmail.com>
2025-01-31 12:41:05 +0800

    LocalActor: as_json update 8

--- django_activitypub/views.py
+++ django_activitypub/views.py
@@ -176,7 +176,7 @@ def profile(request, username):
     data.update(actor.as_json())
 
     url_pattern = re.compile(r'(https?://www\.|https?://)([^\s]+?\.[^\s]+?\b)')
-    domain = url_pattern.findall(request.headers.get("User-Agent")) or url_pattern.findall(request.headers.get("Signature"))
+    domain = url_pattern.findall(request.headers.get("User-Agent", '')) or url_pattern.findall(request.headers.get("Signature", ''))
     if type(domain) is list:
         data['featuredTags'] = list(parse_hashtags('#IndieComics #Gamer #DigitalArt #ArtistOnMastodon', domain[1]))
 
